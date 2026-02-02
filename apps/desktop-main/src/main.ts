539903985ca082fbfe9fced7f5f2f53c2e46324b
Rafael Aquino <rafael.aquinoquijano@gmail.com>
2026-02-02 06:43:40 -0600

    nx:run-commands conf

--- apps/desktop-main/src/main.ts
+++ apps/desktop-main/src/main.ts
@@ -1,1 +1,25 @@
-console.log('Hello World');
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+
+let win: BrowserWindow;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  // En desarrollo carga la URL de nx serve, en prod el archivo físico
+  const isDev = !app.isPackaged;
+  if (isDev) {
+    win.loadURL('http://localhost:4200');
+  } else {
+    win.loadFile(path.join(__dirname, '../frontend-app/browser/index.html'));
+  }
+}
+
+app.on('ready', createWindow);

0bbf14a40fc253cee60634cc696561e9d0231c05
Rafael Aquino <rafael.aquinoquijano@gmail.com>
2026-02-02 06:29:42 -0600

    Configuración Crítica del Frontend

--- apps/desktop-ui/src/app/app.config.ts
+++ apps/desktop-ui/src/app/app.config.ts
@@ -2,9 +2,9 @@ import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(appRoutes)],
+  providers: [provideBrowserGlobalErrorListeners(), provideRouter(appRoutes, withHashLocation())],
 };

23289b7188299c982307bdda8d50a13eb7c04e7d
Rafael Aquino <rafael.aquinoquijano@gmail.com>
2026-02-08 18:13:11 -0600

    embedding disabled

--- apps/api/src/main/kotlin/sv/com/clip/dictionary/infrastructure/DjlEmbeddingAdapter.kt
+++ apps/api/src/main/kotlin/sv/com/clip/dictionary/infrastructure/DjlEmbeddingAdapter.kt
@@ -1,26 +1,47 @@
 package sv.com.clip.dictionary.infrastructure
 
+import ai.djl.huggingface.translator.TextEmbeddingTranslator
+import ai.djl.huggingface.translator.TextEmbeddingTranslatorFactory
 import ai.djl.repository.zoo.Criteria
 import ai.djl.repository.zoo.ZooModel
 import jakarta.annotation.PreDestroy
+import org.springframework.core.io.ClassPathResource
 import org.springframework.stereotype.Service
 import sv.com.clip.dictionary.domain.EmbeddingProvider
+import java.nio.file.Path
+import java.nio.file.Paths
 
 @Service
 class DjlEmbeddingAdapter : EmbeddingProvider {
-  private val model: ZooModel<String, FloatArray> = Criteria.builder()
+/*  private val model: ZooModel<String, FloatArray> = Criteria.builder()
     .setTypes(String::class.java, FloatArray::class.java) // TODO: urgent
     .optModelUrls("djl://ai.djl.huggingface.pytorch/sentence-transformers/all-MiniLM-L6-v2")
     .optEngine("PyTorch")
     .build()
-    .loadModel()
+    .loadModel()*/
+/*val resource = ClassPathResource("models/all-MiniLM-L6-v2")
+  val modelPath: Path = Paths.get(resource.uri)
+  private val model: ZooModel<String, FloatArray> = Criteria.builder()
+    .setTypes(String::class.java, FloatArray::class.java)
+    .optModelUrls("file://${modelPath.toAbsolutePath()}/")
+    // Cambia optModelUrls por optModelPath apuntando a tu carpeta local
+    //.optModelPath(modelPath)
+    //.optModelName("pytorch_model.bin")
+    //.optEngine("PyTorch")
+    // El translator es necesario para que el modelo entienda el input String
+    //.optTranslator(TextEmbeddingTranslator.builder().build())
+    .optTranslatorFactory(TextEmbeddingTranslatorFactory())
+    .optOption("trainable", "false")
+    .build()
+    .loadModel()*/
 
   override fun calculate(term: String): FloatArray {
-    model.newPredictor().use { predictor ->
+    return floatArrayOf()
+/*    model.newPredictor().use { predictor ->
       return predictor.predict(term)
-    }
+    }*/
   }
 
-  @PreDestroy
-  fun close() = model.close()
+/*  @PreDestroy
+  fun close() = model.close()*/
 }
